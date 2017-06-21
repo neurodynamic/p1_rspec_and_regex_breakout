32fc43ce39e47cad4b9c3277f27a736a37f40053
Mark Stewart <markstewart@dbc-markstewart.local>
2017-06-21 10:39:23 -0700

    updates specs

--- spec/phone_numbers_spec.rb
+++ spec/phone_numbers_spec.rb
@@ -1,20 +1,30 @@
 require 'rspec'
 require_relative '../phone_numbers'
 
-describe '#standardize_phone_number' do
-  let(:basic_phone_number) { "(555) 555-5555" }
 
-  it "should not change an already standardized phone number" do
+describe '#standardize_phone_number' do
+
+  let(:basic_phone_number) { "(555) 555-5555" }
+  let(:invalid_number) { "fjhdskhfdjkshjkdsf" }
+  let(:dash_separated_number) { "555-555-5555" }
+
+  it "doesn't change an already standardized phone number" do
     output = standardize_phone_number(basic_phone_number)
 
     expect(output).to eq(basic_phone_number)
   end
 
-  it "should throw an error if there is no valid phone number"
+  it "throws an error if there is no valid phone number" do
+    expect { standardize_phone_number(invalid_number) }.to raise_error(InvalidPhoneNumberError)
+  end
 
-  it "should change XXX-XXX-XXXX numbers to (XXX) XXX-XXXX format"
+  it "changes XXX-XXX-XXXX numbers to (XXX) XXX-XXXX format" do
+    output = standardize_phone_number(dash_separated_number)
 
-  it "should change XXX.XXX.XXXX numbers to (XXX) XXX-XXXX format"
+    expect(output).to eq(basic_phone_number)
+  end
 
-  it "should ignore extra numbers"
+  it "changes XXX.XXX.XXXX numbers to (XXX) XXX-XXXX format"
+
+  it "ignores extra numbers"
 end
